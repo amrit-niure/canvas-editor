c064d9df7cf5125fe76a4f042f3f809d29fc0bc4
Amrit Niure <niureamrit2@gmail.com>
2025-02-09 17:55:33 +1100

    fontst

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Delius, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +10,13 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const delius = Delius({
+  variable: "--font-delius",
+  subsets: ["latin"],
+  weight: ["400"],
+  style: ["normal"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${delius.className} antialiased`}
       >
         {children}
       </body>
